9effb9fe0c2ef5df6d022ac3e15debf1327813ef
jie.zhu <jie12.zhu@getech.cn>
2021-04-01 11:09:15 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <VueDynamicTable/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import VueDynamicTable from './components/VueDynamicTable.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    VueDynamicTable,
   },
 })
 export default class App extends Vue {}
---
+++ src/packages/vue-dynamic-table-head/src/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="vue-dynamic-table">
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="address"
+          label="地址">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import {TableColumn, Table} from 'element-ui'
+
+
+@Component({
+  name: "vue-dynamic-table-head",
+  components: {
+    'el-table': Table,
+    'el-table-column': TableColumn
+  }
+})
+export default class VueDynamicTable extends Vue {
+  tableData = [{
+    date: '2016-05-02',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1518 弄'
+  }, {
+    date: '2016-05-04',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1517 弄'
+  }, {
+    date: '2016-05-01',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1519 弄'
+  }, {
+    date: '2016-05-03',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1516 弄'
+  }];
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
